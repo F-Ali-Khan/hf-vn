c64e4b7a04c68aed6024c1033f4f522e8f8c805b
Fawad <fawad.ali.khan@cern.ch>
2025-11-17 09:46:36 +0500

    update utils.py with new version

--- utils/utils.py
+++ utils/utils.py
@@ -1,50 +1,51 @@
-'''
-Analysis utilities for flow analysis
-'''
-
+import shutil
 import os
 import sys
+import ROOT
 import ctypes
 from itertools import combinations
-import numpy as np
 from PIL import Image
 import math
 import glob
 import re
-import shutil
-import ROOT
 from ROOT import TH1, TH2, TH3
+import numpy as np
 
 def check_dir(dir):
-    if not os.path.exists(dir):
-        print(f"\033[32m{dir} does not exist, it will be created\033[0m")
-        os.makedirs(dir)
-    else:
-        print(f"\033[33m{dir} already exists, it will be overwritten\033[0m")
-        shutil.rmtree(dir)
-        os.makedirs(dir)
-    return
+
+	if not os.path.exists(dir):
+		print(f"\033[32m{dir} does not exist, it will be created\033[0m")
+		os.makedirs(dir)
+	else:
+		print(f"\033[33m{dir} already exists, it will be overwritten\033[0m")
+		shutil.rmtree(dir)
+		os.makedirs(dir)
+
+	return
 
 def logger(message, level='INFO'):
-    """
-    Function to log messages with different levels.
-    """
-    message = f"[{level}] {message}"
-    if level == 'INFO':
-        print(f"\033[32m{message}\033[0m")
-    elif level == 'WARNING':
-        print(f"\033[33m{message}\033[0m")
-    elif level == 'ERROR':
-        print(f"\033[31m{message}\033[0m")
-        sys.exit(1)
-    elif level == 'COMMAND':
-        print(f"\033[35m{message}\033[0m")
-    elif level == 'DEBUG':
-        print(f"\033[34m{message}\033[0m")
-    elif level == 'PAUSE':
-        input(f"\033[36m{message}\n{level}: Press Enter to continue.\033[0m")
-    else:
-        print(f"\033[37m{message}\033[0m")
+	"""
+	Function to log messages with different levels.
+	Args:
+		message (str): The message to log.
+		level (str): The level of the message ('INFO', 'WARNING', 'ERROR').
+	"""
+	message = f"[{level}] {message}"
+	if level == 'INFO':
+		print(f"\033[32m{message}\033[0m")
+	elif level == 'WARNING':
+		print(f"\033[33m{message}\033[0m")
+	elif level == 'ERROR':
+		print(f"\033[31m{message}\033[0m")
+		sys.exit(1)
+	elif level == 'COMMAND':
+		print(f"\033[35m{message}\033[0m")
+	elif level == 'DEBUG':
+		print(f"\033[34m{message}\033[0m")
+	elif level == 'PAUSE':
+		input(f"\033[36m{message}\n{level}: Press Enter to continue.\033[0m")
+	else:
+		print(f"\033[37m{message}\033[0m")  # Default to white for unknown levels
 
 def make_dir_root_file(dir, file):
     if not file.GetDirectory(dir):
@@ -53,6 +54,16 @@ def make_dir_root_file(dir, file):
 def profile_mass_sp(hist_mass_sp, inv_mass_bins, resolution):
     '''
     Profile the mass sparse to get vn versus mass
+    Input:
+        - hist_mass_sp:
+            THnSparse, input THnSparse object (already projected in centrality and pt)
+        - inv_mass_bins:
+            list of floats, bin edges for the mass axis
+        - resolution:
+            float, resolution to normalize the vn values
+    Output:
+        - hist_vn_vs_mass:
+            TH1D, histogram with vn as a function of mass
     '''
     hist_vn_vs_mass = ROOT.TH1D('hist_vn_vs_mass', 'hist_vn_vs_mass', len(inv_mass_bins)-1, np.array(inv_mass_bins))
     hist_vn_vs_mass.SetDirectory(0)
@@ -69,7 +80,24 @@ def profile_mass_sp(hist_mass_sp, inv_mass_bins, resolution):
 def get_vn_versus_mass(thnSparses, resolutions, inv_mass_bins, mass_axis, vn_axis, sampling=-1, debug=False):
     '''
     Project vn versus mass
+
+    Input:
+        - thnSparse:
+            THnSparse, input THnSparse obeject (already projected in centrality and pt)
+        - inv_mass_bins:
+            list of floats, bin edges for the mass axis
+        - mass_axis:
+            int, axis number for mass
+        - vn_axis:
+            int, axis number for vn
+        - debug:
+            bool, if True, create a debug file with the projections (default: False)
+
+    Output:
+        - hist_mass_proj:
+            TH1D, histogram with vn as a function of mass
     '''
+
     invmass_bins = np.array(inv_mass_bins)
 
     if sampling != -1:
@@ -100,7 +128,60 @@ def get_vn_versus_mass(thnSparses, resolutions, inv_mass_bins, mass_axis, vn_axi
 
 def get_vnfitter_results(vnFitter, secPeak, useRefl, useTempl):
     '''
-    Get vn fitter results
+    Get vn fitter results:
+    0: BkgInt
+    1: BkgSlope
+    2: SgnInt
+    3: Mean
+    4: Sigma
+    5: SecPeakInt
+    6: SecPeakMean
+    7: SecPeakSigma
+    8: ConstVnBkg
+    9: SlopeVnBkg
+    10: v2Sgn
+    11: v2SecPeak
+    12: reflection
+
+    Input:
+        - vnfitter:
+            VnVsMassFitter, vn fitter object
+        - secPeak:
+            bool, if True, save secondary peak results
+        - useRefl:
+            bool, if True, save the results with reflection
+
+    Output:
+        - vn_results:
+            dict, dictionary with vn results
+            vn: vn value
+            vnUnc: uncertainty of vn value
+            mean: mean value
+            meanUnc: uncertainty of mean value
+            sigma: sigma value
+            sigmaUnc: uncertainty of sigma value
+            ry: raw yield
+            ryUnc: uncertainty of raw yield
+            ryTrue: true raw yield
+            ryTrueUnc: uncertainty of true raw yield
+            signif: significance
+            signifUnc: uncertainty of significance
+            chi2: reduced chi2
+            prob: fit probability
+            fTotFuncMass: total fit function for mass
+            fTotFuncVn: total fit function for vn
+            secPeakMeanMass: secondary peak mean mass
+            secPeakMeanMassUnc: uncertainty of secondary peak mean mass
+            secPeakSigmaMass: secondary peak sigma mass
+            secPeakSigmaMassUnc: uncertainty of secondary peak sigma mass
+            secPeakMeanVn: secondary peak mean vn
+            secPeakMeanVnUnc: uncertainty of secondary peak mean vn
+            secPeakSigmaVn: secondary peak sigma vn
+            secPeakSigmaVnUnc: uncertainty of secondary peak sigma vn
+            vnSecPeak: vn secondary peak
+            vnSecPeakUnc: uncertainty of vn secondary peak
+            fMassRflFunc: mass reflection function
+            fMassBkgRflFunc: mass background reflection function
     '''
     vn_results = {}
     vn_results['vn'] = vnFitter.GetVn()
@@ -179,20 +260,35 @@ def get_vnfitter_results(vnFitter, secPeak, useRefl, useTempl):
 def get_particle_info(particleName):
     '''
     Get particle information
+
+    Input:
+        - particleName: 
+            the name of the particle
+
+    Output:
+        - particleTit: 
+            the title of the particle
+        - massAxisTit: 
+            the title of the mass axis
+        - decay: 
+            the decay of the particle
+        - massForFit: 
+            float, the mass of the particle
     '''
+
     if particleName == 'Dplus':
         particleTit = 'D^{+}'
         massAxisTit = '#it{M}(K#pi#pi) (GeV/#it{c}^{2})'
         massForFit = ROOT.TDatabasePDG.Instance().GetParticle(411).Mass()
         decay = 'D^{+} #rightarrow K^{#minus}#pi^{+}#pi^{+}'
-        massSecPeak = ROOT.TDatabasePDG.Instance().GetParticle(413).Mass()
+        massSecPeak = ROOT.TDatabasePDG.Instance().GetParticle(413).Mass() # D* mass
         secPeakLabel = 'D^{*+}'
     elif particleName == 'Ds':
         particleTit = 'D_{s}^{+}'
         massAxisTit = '#it{M}(KK#pi) (GeV/#it{c}^{2})'
         decay = 'D_{s}^{+} #rightarrow #phi#pi^{+} #rightarrow K^{+}K^{#minus}#pi^{+}'
         massForFit = ROOT.TDatabasePDG.Instance().GetParticle(431).Mass()
-        massSecPeak = ROOT.TDatabasePDG.Instance().GetParticle(411).Mass()
+        massSecPeak = ROOT.TDatabasePDG.Instance().GetParticle(411).Mass() # D+ mass
         secPeakLabel = 'D^{+}'
     elif particleName == 'LctopKpi':
         particleTit = '#Lambda_{c}^{+}'
@@ -202,7 +298,8 @@ def get_particle_info(particleName):
     elif particleName == 'LctopK0s':
         massAxisTit = '#it{M}(pK^{0}_{s}) (GeV/#it{c}^{2})'
         decay = '#Lambda_{c}^{+} #rightarrow pK^{0}_{s}'
-        massForFit = 2.25
+        massForFit = 2.25 # please calfully check the mass of Lc->pK0s, it is constant
+        # massForFit = ROOT.TDatabasePDG.Instance().GetParticle(4122).Mass()
     elif particleName == 'Dstar':
         particleTit = 'D^{*+}'
         massAxisTit = '#it{M}(K#pi#pi) - #it{M}(K#pi) (GeV/#it{c}^{2})'
@@ -218,7 +315,7 @@ def get_particle_info(particleName):
         massAxisTit = '#it{M}(pK#pi) (GeV/#it{c}^{2})'
         decay = 'X_{c}^{+} #rightarrow pK^{#minus}#pi^{+}'
         massForFit = ROOT.TDatabasePDG.Instance().GetParticle(4132).Mass()
-        massSecPeak = ROOT.TDatabasePDG.Instance().GetParticle(4122).Mass()
+        massSecPeak = ROOT.TDatabasePDG.Instance().GetParticle(4122).Mass() # Lc mass
         secPeakLabel = '#Lambda_{c}^{+}'
     else:
         print(f'ERROR: the particle "{particleName}" is not supported! Choose between Dzero, Dplus, Ds, Dstar, and Lc. Exit!')
@@ -229,12 +326,33 @@ def get_particle_info(particleName):
 def check_file_exists(file_path):
     '''
     Check if file exists
+
+    Input:
+        - file_path:
+            str, file path
+
+    Output:
+        - file_exists:
+            bool, if True, file exists
     '''
-    return os.path.exists(file_path)
+    file_exists = False
+    if os.path.exists(file_path):
+        file_exists = True
+    return file_exists
 
 def check_histo_exists(file, histo_name):
     '''
     Check if histogram exists in file
+
+    Input:
+        - file:
+            TFile, ROOT file
+        - histo_name:
+            str, histogram name
+
+    Output:
+        - histo_exists:
+            bool, if True, histogram exists
     '''
     if not check_file_exists(file):
         return False
@@ -247,6 +365,24 @@ def check_histo_exists(file, histo_name):
 def get_refl_histo(reflFile, centMinMax, ptMins, ptMaxs):
     '''
     Method that loads MC histograms for the reflections of D0
+
+    Input:
+        - reflFile:
+           TFile, ROOT file, include reflections of D0
+        - centMinMax:
+            list, min and max centrality
+        - ptMins:
+            list, min pt bins
+        - ptMaxs:
+            list, max pt bins
+    
+    Output:
+        - useRefl:
+            bool, if True, MC histograms for the reflections of D0 exists
+        - hMCSgn:
+            lsit, signal histograms of D0
+        - hMCRefl:
+            list, reflection histograms of D0
     '''
     hMCSgn, hMCRefl = [], []
     if not check_file_exists(reflFile):
@@ -279,6 +415,16 @@ def get_refl_histo(reflFile, centMinMax, ptMins, ptMaxs):
 def get_centrality_bins(centrality):
     '''
     Get centrality bins
+
+    Input:
+        - centrality:
+            str, centrality class (e.g. 'k3050')
+
+    Output:
+        - cent_bins:
+            list of floats, centrality bins
+        - cent_label:
+            str, centrality label
     '''
     if centrality == 'k05':
         return '0_5', [0, 5]
